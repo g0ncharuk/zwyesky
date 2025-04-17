659fc7f44339bb6c9fed8ba2c5164c01005f5302
g0ncharuk <andrey.honcharuk@tateeda.com>
2025-04-18 01:35:04 +0300

    Update GitHub Actions workflow for GitHub Pages deployment and set base path in Vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import javascriptObfuscator from "rollup-plugin-javascript-obfuscator";
 
 export default defineConfig({
+  base: "/zwyesky/",
   plugins: [
     {
       ...javascriptObfuscator({}),
